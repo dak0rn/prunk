fbeb7392fd30ab8f92dc644bae0e5b0920672eab
dak0rn <daniel@suitsoft.eu>
2015-12-15 21:47:46 +0100

    Add tests for mock()

--- test/smock.js
+++ test/smock.js
@@ -37,3 +37,43 @@ describe('Export', function() {
     });
 
 });
+
+// mock function
+describe('mock()', function() {
+
+    beforeEach(function() {
+        // Reset the require cache
+        require.cache = {};
+    });
+
+    it('should take two arguments', function() {
+        expect(smock.mock.length).to.equal(2);
+    });
+
+    it('should mock correctly with a string', function() {
+        smock.mock('blah', 42);
+
+        var imp = require('blah');
+        expect( imp ).to.equal(42);
+    });
+
+    it('should mock correctly with a regex', function() {
+        smock.mock(/^blah/, 42);
+
+        ['blah', 'blah1', 'blah2', 'blahblah'].forEach(function(str) {
+            var imp = require(str);
+            expect(imp).to.equal(42);
+        });
+    });
+
+    it('should correctly mock using callbacks', function() {
+        var filter = function(str) { return 0 === str.indexOf('blah'); };
+        smock.mock(filter, 42);
+
+        ['blah', 'blah1', 'blah2', 'blahblah'].forEach(function(str) {
+            var imp = require(str);
+            expect(imp).to.equal(42);
+        });
+    });
+
+});
