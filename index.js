36c15769dff14f0097480b582475f70dcbfd9b93
dak0rn <daniel@suitsoft.eu>
2016-03-31 07:45:58 +0200

    Yeah, the license.

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 // # prunk
 // ## A mocking utility for node.js require
 //
-//     Author: Daniel Koch <daniel@suitsoft.eu>
-//     Version: 1.0.0
+//     Author: Daniel Koch
+//     Version: 1.2.1
 //
+//     Copyright 2016 Daniel Koch
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
 
 // Require node's `module` module that provides
 // access to all the things `require()` uses
